92560f7dbe67903c7cd6d087f7c2df28adae0673
Aymezia <jorisou.san@gmail.com>
2026-05-01 23:24:54 +0200

    Update __init__.py

--- network_monitor/__init__.py
+++ network_monitor/__init__.py
@@ -7,8 +7,8 @@ Site web: https://Aymzia.cc
 
 __version__ = "1.0.0"
 __author__ = "Aymezia"
-__email__ = "contact@aymzia.cc"
-__website__ = "https://Aymzia.cc"
+__email__ = "contact@aymezia.cc"
+__website__ = "https://Aymezia.cc"
 __description__ = "Analyseur de Trafic Réseau Ultra-Complet"
 __license__ = "MIT"
 __copyright__ = "Copyright © 2026 Aymezia. Tous droits réservés."
